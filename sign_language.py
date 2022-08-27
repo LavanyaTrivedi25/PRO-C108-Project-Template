6de740ccd860199eda82f73e8b89a47c83fe215f
Lavanya Trivedi <85881382+LavanyaTrivedi25@users.noreply.github.com>
2022-08-27 16:47:57 +0530

    Update sign_language.py

--- sign_language.py
+++ sign_language.py
@@ -24,8 +24,30 @@ while True:
                 lm_list.append(lm)
 
              #Code goes here   
+    for tip in finger_tips:
+        #getting the landmark tip position and drawing blue circles
+        x,y = int(lm_list[tip].x*w), ,y = int(lm_list[tip].y*h)
+        cv2.circle(img, (x,y), 15, (0,255,0), cv2.FILLED)
+        
+    finger_fold_status = []
+    
+     # if finger folded changing color to green
+    if lm_list[tip].x < lm_list[tip - 3].x:
+        cv2.circle(img, (x,y), 15, (0,255,0), cv2.FILLED)
+        finger_fold_status.append(True)
+    else:
+        finger_fold_status.append(False)
 
-
+    
+    if all(finger_fold_status):
+        #checking if the thumb is up
+        if lm_list[thumb_tip].y < lm_list[thumb_tip - 1].y < lm_list[thumb_tip - 2].y:
+            print("Like")
+            cv2.putText(img, "Like", (20,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 3)
+         #checking if the thumb is DOWN
+        if lm_list[thumb_tip].y > lm_list[thumb_tip - 1].y > lm_list[thumb_tip - 2].y:
+            print("Dislike")
+            cv2.putText(img, "Dislike", (20,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 3)
 
             mp_draw.draw_landmarks(img, hand_landmark,
             mp_hands.HAND_CONNECTIONS, mp_draw.DrawingSpec((0,0,255),2,2),
